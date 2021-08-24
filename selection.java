8400c4faac8d9661c985e3de9c3604ed7e91191a
SAURABH YADAV <72190873+saurabhyadav3615@users.noreply.github.com>
2021-08-24 20:46:25 +0530

    Add files via upload

--- selection.java
+++ selection.java
@@ -1,34 +1,20 @@
-import java.util.Arrays;
-
 class selection {
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 1, 2, 3 };
-        selection(arr);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    static void selection(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            // find the max item in the remaining array and swap with correct index
-            int last = arr.length - i - 1;
-            int maxIndex = getMaxIndex(arr, 0, last);
-            swap(arr, maxIndex, last);
-        }
-    }
-
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
-
-    static int getMaxIndex(int[] arr, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[max] < arr[i]) {
-                max = i;
+        int[] a = { 78, 52, 12, 45 };
+        int min, temp = 0;
+        for (int i = 0; i < a.length; i++) {
+            min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
             }
+            temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
         }
-        return max;
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
     }
 }
